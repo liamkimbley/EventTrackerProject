11f1497e771f895ddb02a2719c624bf68f80419d
liamkimbley <liam.kimbley@gmail.com>
2018-09-22 15:33:05 -0600

    project updated

--- EventTrackerREST/src/main/webapp/js/script.js
+++ EventTrackerREST/src/main/webapp/js/script.js
@@ -1,127 +1,219 @@
-window.addEventListener('load', function(e) {
-	console.log("loaded");
+window.addEventListener('load', pageLoad);
 
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', "/api/expenses");
-	// true can be left off. it will default to true. but don't set to false ===
-	// response blocking
+function pageLoad(e) {
+  console.log("loaded");
 
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4) {
-			if (xhr.status === 200) {
-				var data = JSON.parse(xhr.responseText);
-				let div = document.getElementById('divId');
-				let table = document.createElement('table');
-				let head = document.createElement('thead'); // tr and th
-				let trh = document.createElement('tr');
-				let th = document.createElement('th');
-				let th2 = document.createElement('th');
-				let th3 = document.createElement('th');
-				let th4 = document.createElement('th');
-				let th5 = document.createElement('th');
-				let tbody = document.createElement('tbody'); // tr and td
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "/api/expenses");
+  // true can be left off. it will default to true. but don't set to false ===
+  // response blocking
 
-				th.textContent = "Date";
-				trh.appendChild(th);
-				th2.textContent = "Name"
-				trh.appendChild(th2);
-				th3.textContent = "Price"
-				trh.appendChild(th3);
-				th4.textContent = "Reason"
-				trh.appendChild(th4);
-				th5.textContent = "Description"
-				trh.appendChild(th5);
-				head.appendChild(trh);
-				table.appendChild(head);
-				for (let i = 0; i < data.length; i++) {
-// anything that does anything with the data returned MUST be within this code block!!!!!
-					let tr = document.createElement('tr');
-					let td = document.createElement('td');
-					let td2 = document.createElement('td');
-					let td3 = document.createElement('td');
-					let td4 = document.createElement('td');
-					let td5 = document.createElement('td');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        let div = document.getElementById('divId');
+        let table = document.createElement('table');
+        let head = document.createElement('thead'); // tr and th
+        let trh = document.createElement('tr');
+        let th = document.createElement('th');
+        let th2 = document.createElement('th');
+        let th3 = document.createElement('th');
+        let th4 = document.createElement('th');
+        let th5 = document.createElement('th');
+        let th6 = document.createElement('th');
+        let tbody = document.createElement('tbody'); // tr and td
+				let total = 0.00;
 
-					let str = "" + data[i].date.toString();
-					td.textContent = str.substr(0,10);
-					tr.appendChild(td);
-					td2.textContent = data[i].name;
-					tr.appendChild(td2);
-					td3.textContent = "$" + data[i].price;
-					tr.appendChild(td3);
-					td4.textContent = data[i].reason;
-					tr.appendChild(td4);
-					td5.textContent = data[i].description;
-					tr.appendChild(td5);
-					tbody.appendChild(tr);
+				table.name ="table";
+				th6.textContent = "ID";
+				trh.appendChild(th6);
+        th.textContent = "Date";
+        trh.appendChild(th);
+        th2.textContent = "Name"
+        trh.appendChild(th2);
+        th3.textContent = "Price"
+        trh.appendChild(th3);
+        head.appendChild(trh);
+        table.appendChild(head);
+        for (let i = 0; i < data.length; i++) {
+          // anything that does anything with the data returned MUST be within this code block!!!!!
+					let prc = parseFloat(data[i].price);
+					if(!isNaN(prc)){
+						total += prc;
+					};
 
-				}
-				console.log(data);
-				table.appendChild(tbody);
-				div.appendChild(table);
-			} else if (xhr.status !== 200) {
-				console.error(xhr.status + ': ' + xhr.responseText);
-			}
-		}
-	};
-	xhr.send(null);
+          let tr = document.createElement('tr');
+          let td = document.createElement('td');
+          let td2 = document.createElement('td');
+          let td3 = document.createElement('td');
+          let td6 = document.createElement('td');
 
-});
+					td6.textContent = data[i].id;
+					tr.appendChild(td6);
+          let str = "" + data[i].date.toString();
+          td.textContent = str.substr(0, 10);
+          tr.appendChild(td);
+          td2.textContent = data[i].name;
+          tr.appendChild(td2);
+          td3.textContent = "$" + data[i].price;
+          tr.appendChild(td3);
+          tbody.appendChild(tr);
+
+					tr.addEventListener("click", function(e) {
+						console.log("table");
+
+								 let element = e.target;
+								 let id = element.id;
+								 let pdiv = document.getElementById("detail");
+
+								 document.getElementById("date").textContent = "Date: " + str.substr(0, 10);
+								 document.getElementById("name").textContent = data[i].name;
+								 document.getElementById("price").textContent = "Amount Spent: $" + data[i].price;
+								 document.getElementById("reason").textContent ="Reason: " + data[i].reason;
+								 document.getElementById("desc").textContent = "Description: " + data[i].description;
+
+								 if (pdiv.lastElementChild.name !== "delete"){
+									 let subInput = document.createElement("input");
+								   subInput.name = "edit";
+								   subInput.type = "submit";
+								   subInput.value = "Edit Log";
+								   pdiv.appendChild(subInput);
+									 let delInput = document.createElement("input");
+								   delInput.name = "delete";
+								   delInput.type = "submit";
+								   delInput.value = "Delete Log";
+								   pdiv.appendChild(delInput);
+							 }
+					 })
+        }
+        console.log(data);
+        table.appendChild(tbody);
+        div.appendChild(table);
+				document.getElementById("total").textContent = "Total Spent: $" + total;
+      } else if (xhr.status !== 200) {
+        console.error(xhr.status + ': ' + xhr.responseText);
+      }
+    }
+  };
+  xhr.send(null);
+
+};
 
 var btn = document.getElementById('btn1');
 btn1.addEventListener("click", showForm);
 
 
-function showForm(e){
-	btn1.removeEventListener("click", showForm);
-	let divForm = document.getElementById("formDiv");
-	let form = document.createElement("form");
-	form.name = "addForm";
-	let nameInput = document.createElement("input");
-	nameInput.name = "name";
-	nameInput.type = "text";
-	nameInput.placeholder = "Name of Expense"
-	form.appendChild(nameInput);
-	let priceInput = document.createElement("input");
-	priceInput.name = "price";
-	priceInput.type = "text";
-	priceInput.placeholder = "Price"
-	form.appendChild(priceInput);
-	let resInput = document.createElement("input");
-	resInput.name = "reason";
-	resInput.type = "text";
-	resInput.placeholder = "Reason"
-	form.appendChild(resInput);
-	let descInput = document.createElement("input");
-	descInput.name = "description";
-	descInput.type = "text";
-	descInput.placeholder = "Description"
-	form.appendChild(descInput);
-	let subInput = document.createElement("input");
-	subInput.name = "submit";
-	subInput.type = "submit";
-	subInput.value = "Submit";
-	form.appendChild(subInput);
-	divForm.appendChild(form);
+function showForm(e) {
+  btn1.removeEventListener("click", showForm);
+  let divForm = document.getElementById("formDiv");
+  let form = document.createElement("form");
+  form.name = "addForm";
+  let nameInput = document.createElement("input");
+  nameInput.name = "name";
+  nameInput.type = "text";
+  nameInput.placeholder = "Name of Expense"
+  form.appendChild(nameInput);
+  let priceInput = document.createElement("input");
+  priceInput.name = "price";
+  priceInput.type = "text";
+  priceInput.placeholder = "Price"
+  form.appendChild(priceInput);
+  let resInput = document.createElement("input");
+  resInput.name = "reason";
+  resInput.type = "text";
+  resInput.placeholder = "Reason"
+  form.appendChild(resInput);
+  let descInput = document.createElement("input");
+  descInput.name = "description";
+  descInput.type = "text";
+  descInput.placeholder = "Description"
+  form.appendChild(descInput);
+  let subInput = document.createElement("input");
+  subInput.name = "submit";
+  subInput.type = "submit";
+  subInput.value = "Submit";
+  form.appendChild(subInput);
+  divForm.appendChild(form);
 
 };
 
-document.addEventListener('click',function(e){
-	if (e.target && e.target.name === 'submit'){
-		divForm.addForm.submit.addEventListener("click", submitBtn); // ********************
-	}
-});
+document.addEventListener('click', function(e) {
+  if (e.target && e.target.name === 'submit') {
+    e.preventDefault();
+    console.log("Submit");
+    let name = formDiv.firstElementChild.name.value;
+    let price = formDiv.firstElementChild.price.value;
+    let reason = formDiv.firstElementChild.reason.value;
+    let desc = formDiv.firstElementChild.description.value;
 
-function submitBtn(e){
-	e.preventDefault();
-	let btn = e.target;
-	btn.addEventListener("click", function(e){
-		btn1.addEventListener("click", showForm);
-		let name = divForm.addForm.name.value;
-		let price = divForm.addForm.price.value;
-		let reason = divForm.addForm.reason.value;
-		let desc = divForm.addForm.description.value;
+    let pre = parseFloat(price);
+    let err = formPop(name, pre);
+
+    if (err.length > 0) {
+      let divForm = document.getElementById("formDiv");
+      let ol = document.createElement("ol");
+
+      for (let i = 0; i < err.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = err[i];
+        li.style.color = "red";
+        ol.appendChild(li);
+        divForm.nextElementSibling.appendChild(ol);
+        // console.log(err[i]);
+      }
+    } else {
+      // console.log(name);
+      // console.log(reason);
+      // let form = ;
+			// console.log(form);
+			let formJson = JSON.stringify({
+				"name": name,
+				"price": price,
+				"description": desc,
+				"reason": reason
+			});
+      var xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
+			console.log(formJson);
+      xhr.open('POST', "/api/expenses");
+ 			xhr.setRequestHeader("Content-Type", "application/json");
+			console.log("1 " +xhr.readyState); //***********
+			xhr.send(formJson);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+					console.log("2 " +xhr.readyState); //***********
+
+          if (xhr.status === 201) {
+						console.log("3 " +xhr.readyState); //***********
+						document.location.reload();
+
+          } else if (xhr.status !== 200) {
+            console.error(xhr.status + ': ' + xhr.responseText);
+          }
+        }
+      };
+    }
+		if (e.target && e.target.name === 'edit') {
+	    e.preventDefault();
+			console.log("edit");
+		}
+		if (e.target && e.target.name === 'delete') {
+	    e.preventDefault();
+			console.log("delete");
+		}
+
+  };
+}); // ********************
 
 
-	});
+function formPop(str, dub) {
+  let arr = [];
+  if (str === "") {
+    arr.push("Name Field Can Not Be Blank")
+  };
+  if (isNaN(dub)) {
+    arr.push("Please Enter A Valid Price")
+  };
+  return arr;
 };
